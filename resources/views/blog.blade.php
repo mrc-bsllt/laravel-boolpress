ec8cc14720e7e5d6dc96fdae844df1f9075c532f
MarcoB <mrc.bsllt@gmail.com>
2021-02-18 16:29:33 +0100

    stampato tutti i posts nella pagina Blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,1 +1,16 @@
 @extends("layout.layout-main")
+
+@section("main-content")
+  <div class="container cards">
+    @foreach ($posts as $post)
+      <div class="card">
+        <div class="card_content">
+          <img src="https://www.webnode.it/blog/files/2019/05/blogger-di-successo.png" alt="">
+          <a href="#">
+            <h3>{{ $post->title }}</h3>
+          </a>
+        </div>
+      </div>
+    @endforeach
+  </div>
+@endsection

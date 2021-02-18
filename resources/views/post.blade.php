b97bdfb482385b0f3ee90fa80b8fb0465efa4fa7
MarcoB <mrc.bsllt@gmail.com>
2021-02-18 17:00:17 +0100

    aggiunta la pagina di dettaglio con i commenti visualizzati

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,1 +1,25 @@
 @extends("layout.layout-main")
+
+@section("main-content")
+  <div class="flex_container">
+    <div class="container post-content">
+      <h2 class="text-center">{{ $post->title }} <small>({{ $post->author }})</small></h2>
+      <p>{{ $post->text }}</p>
+      <div class="comments">
+        <h4>COMMENTI:</h4>
+
+        <div class="list-section">
+          <ul>
+            @foreach ($post->comments as $comment)
+              <li>
+                <small><strong>{{ $comment->author }}</strong></small>
+                <p>{{ $comment->content }}</p>
+              </li>
+            @endforeach
+          </ul>
+        </div>
+        
+      </div>
+    </div>
+  </div>
+@endsection

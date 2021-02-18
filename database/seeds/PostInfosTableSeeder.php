6b6cfdceafa07db5efc0fdc2cc47c12fa3265aae
MarcoB <mrc.bsllt@gmail.com>
2021-02-18 14:59:09 +0100

    popolato la tabella postInfos e aggiunto la relazione con la tabella posts

---
+++ database/seeds/PostInfosTableSeeder.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\PostInfo;
+use App\Post;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
+
+
+class PostInfosTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+      $posts = Post::all();
+
+      foreach ($posts as $post) {
+        $newPostInfo = new PostInfo();
+
+        $newPostInfo->post_id = $post->id;
+        $newPostInfo->slug = Str::slug($post->title, '-');
+        $newPostInfo->status = $faker->randomElement(['public', 'private', 'draft']);
+        $newPostInfo->visits = $faker->randomDigit();
+
+        $newPostInfo->save();
+      }
+    }
+}

8d7d51e90f5c5f151648dedc2d2b9018e72ac1b7
MarcoB <mrc.bsllt@gmail.com>
2021-02-18 14:46:30 +0100

    creato la tabella post_infos

---
+++ database/migrations/2021_02_18_133633_create_post_infos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostInfosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_infos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("post_id");
+            $table->string("slug");
+            $table->string("status", 7);
+            $table->integer('visits');
+
+            // relazione con il Database
+            $table->foreign('post_id')
+             ->references('id')
+             ->on('posts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_infos');
+    }
+}

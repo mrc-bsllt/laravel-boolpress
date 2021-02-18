4dc5a06802f0a2bea6023f974a4fc73c0ce1c961
MarcoB <mrc.bsllt@gmail.com>
2021-02-18 16:04:52 +0100

    creata la home page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ asset("css/app.css") }}">
-        <link rel="stylesheet" href="{{ asset("css/style.css") }}">
-        <title>Laravel</title>
+@extends("layout.layout-main")
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-    </head>
-    <body>
-      <header>
-
-      </header>
-      <main>
-        <ul>
-          @foreach ($posts as $post)
-            <li>{{ $post->comments[0]->content }}</li>
-          @endforeach
-        </ul>
-      </main>
-    </body>
-</html>
+@section("main-content")
+  <div class="container flex_container">
+    <h1>Il mio blog</h1>
+  </div>
+@endsection

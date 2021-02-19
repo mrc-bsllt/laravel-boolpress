14d9a39a1e4515b99861a455c163353161780cc8
MarcoB <mrc.bsllt@gmail.com>
2021-02-19 09:10:26 +0100

    aggiunto condizione || per la classe active di Blog (header)

--- resources/views/layout/layout-main.blade.php
+++ resources/views/layout/layout-main.blade.php
@@ -13,7 +13,7 @@
           <li class="{{ Route::currentRouteName() == "home" ? "active" : "" }}">
             <a href="{{ route("home") }}">Home</a>
           </li>
-          <li class="{{ Route::currentRouteName() == "blog" ? "active" : "" }}">
+          <li class="{{ (Route::currentRouteName() == "blog") || Route::currentRouteName() == "post" ? "active" : "" }}">
             <a href="{{ route("blog") }}">Blog</a>
           </li>
         </ul>

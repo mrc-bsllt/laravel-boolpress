9623861d06e3bf573a86410f880504ace5ad7c95
MarcoB <mrc.bsllt@gmail.com>
2021-02-20 01:35:26 +0100

    creato la show della crud

---
+++ resources/views/crud-articles/show.blade.php
@@ -0,0 +1,39 @@
+@extends("../layout.crud-main")
+
+@section("crud-content")
+  <section id="post_detail">
+    <img src="{{ $post->image }}" alt="{{ $post->title }}">
+
+    <table class="table table-dark table-bordered table-stripped my-3">
+      <tr>
+        <th class="thead-dark">Titolo</th>
+        <td>{{ $post->title }}</td>
+      </tr>
+      <tr>
+        <th class="thead-dark">Testo</th>
+        <td>{{ $post->text }}</td>
+      </tr>
+      <tr>
+        <th class="thead-dark">Autore</th>
+        <td>{{ $post->author }}</td>
+      </tr>
+      <tr>
+        <th class="thead-dark">Stato</th>
+        <td>
+          <span>{{ $post->infoPost->status }}</span>
+          <i class="fas fa-circle ml-1 {{ $post->infoPost->status == "public" ? "green" : "red" }}"></i>
+        </td>
+      </tr>
+      <tr>
+        <th class="thead-dark">Visite</th>
+        <td>{{ $post->infoPost->visits }}</td>
+      </tr>
+      <tr>
+        <th class="thead-dark">Commenti</th>
+        <td>{{ count($post->comments) }}</td>
+      </tr>
+    </table>
+
+    <a class="btn btn-primary" href="{{ route("crud-articles.index") }}">Indietro</a>
+  </section>
+@endsection

e4ae22f35fa85292da4146c6111bb9ad7892d7ac
MarcoB <mrc.bsllt@gmail.com>
2021-02-21 00:11:17 +0100

    sistemato css e sass della pagina di dettaglio dei post

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -1,46 +1,50 @@
 @extends("layout.layout-main")
 
 @section("main-content")
-  <div class="flex_container">
-    <div class="container post-content">
-      <div class="header text-center">
-        <img src="{{ $post->image }}" alt="{{ $post->title }}">
-        <h2>{{ $post->title }}</h2>
-      </div>
-      <p>{{ $post->text }}</p>
-      <small>({{ $post->author }})</small>
-      <div class="comments">
-        <h4>COMMENTI:</h4>
+  <section id="article" class="container py-5">
 
-        <div class="list-section">
-          <ul>
-            @foreach ($post->comments as $comment)
-              <li>
-                <small><strong>{{ $comment->author }}</strong></small>
-                <p>{{ $comment->content }}</p>
-              </li>
-            @endforeach
-          </ul>
-        </div>
-
-        <div class="add-comment">
-          <form action="{{ route("add-comment", $post->id) }}" method="post">
-            @csrf
-            @method("POST")
-
-            <div class="form-group">
-              <label for="author">Username</label>
-              <input type="text" class="form-control" id="author" name="author" value="">
-            </div>
-            <div class="form-group">
-              <label for="content">Inserisci un commento</label>
-              <textarea type="text" class="form-control" id="content" name="content"></textarea>
-            </div>
-            <button type="submit" class="btn btn-success" name="button">Inserisci!</button>
-          </form>
-
-        </div>
-      </div>
+    {{-- IMMAGINE E TITOLO DEL POST --}}
+    <div class="header text-center">
+      <img src="{{ $post->image }}" alt="{{ $post->title }}">
+      <h2 class="my-4">{{ $post->title }}</h2>
     </div>
-  </div>
+
+    {{-- TESTO DELL'ARTICOLO E AUTORE --}}
+    <p>{{ $post->text }}</p>
+    <small class="author"><strong><em>{{ $post->author }}</em></strong></small>
+    <hr>
+
+    {{-- SEZIONE DEI COMMENTI --}}
+    <h4><strong>COMMENTI</strong></h4>
+
+    <div class="list-section">
+      <ul>
+        @foreach ($post->comments as $comment)
+          <li>
+            <small class="author"><strong>{{ $comment->author }}</strong></small>
+            <p>{{ $comment->content }}</p>
+          </li>
+        @endforeach
+      </ul>
+    </div>
+
+    {{-- FORM PER AGGIUNGERE UN COMMENTO --}}
+    <div class="add-comment">
+      <form action="{{ route("add-comment", $post->id) }}" method="post">
+        @csrf
+        @method("POST")
+
+        <div class="form-group">
+          <label for="author">Username</label>
+          <input type="text" class="form-control" id="author" name="author" value="">
+        </div>
+        <div class="form-group">
+          <label for="content">Inserisci un commento</label>
+          <textarea type="text" class="form-control" id="content" name="content"></textarea>
+        </div>
+        <button type="submit" class="btn btn-success" name="button">Inserisci!</button>
+      </form>
+
+    </div>
+  </section>
 @endsection

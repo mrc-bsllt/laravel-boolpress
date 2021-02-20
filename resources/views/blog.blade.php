474e3b7707a11543b09ee927b776607df9e5991b
MarcoB <mrc.bsllt@gmail.com>
2021-02-20 23:29:20 +0100

    sistemato css e sass della pagina blog

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -1,16 +1,16 @@
 @extends("layout.layout-main")
 
 @section("main-content")
-  <div class="container cards">
+  <div class="container d-flex wrap">
     @foreach ($posts as $post)
-      <div class="card">
+      <a class="card" href="{{ route("post", ["slug" => $post->infoPost->slug]) }}">
+
         <div class="card_content">
           <img src="{{ $post->image }}" alt="{{ $post->title }}">
-          <a href="{{ route("post", ["slug" => $post->infoPost->slug]) }}">
-            <h3>{{ $post->title }}</h3>
-          </a>
+          <h3 class="text-center">{{ $post->title }}</h3>
         </div>
-      </div>
+
+      </a>
     @endforeach
   </div>
 @endsection
--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -1,9 +1,9 @@
 <header>
   <ul class="d-flex justify-content-center align-items-center">
-    <li class="{{ Route::currentRouteName() == "home" ? "active" : "" }}">
+    <li class="{{ Route::currentRouteName() == "home" ? "active" : "" }} mx-1">
       <a href="{{ route("home") }}">Home</a>
     </li>
-    <li class="{{ (Route::currentRouteName() == "blog") || Route::currentRouteName() == "post" ? "active" : "" }}">
+    <li class="{{ (Route::currentRouteName() == "blog") || Route::currentRouteName() == "post" ? "active" : "" }} mx-1">
       <a href="{{ route("blog") }}">Blog</a>
     </li>
   </ul>

48902cceccc70e94b55f685b660f69efd25c407f
MarcoB <mrc.bsllt@gmail.com>
2021-02-22 13:56:12 +0100

    creato la tabella e il model dei tags

--- app/Post.php
+++ app/Post.php
@@ -21,4 +21,9 @@ class Post extends Model
   public function comments() {
     return $this->hasMany("App\Comment");
   }
+
+  public function tags() {
+    return $this->belongsToMany("App\Tag");
+  }
+
 }

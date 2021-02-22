90448f870028f410c0c859050ca1cf1d979edfdd
MarcoB <mrc.bsllt@gmail.com>
2021-02-22 14:47:04 +0100

    Aggiunto i tags alla vista show della crud

--- resources/views/crud-articles/show.blade.php
+++ resources/views/crud-articles/show.blade.php
@@ -32,6 +32,19 @@
         <th class="thead-dark">Commenti</th>
         <td>{{ count($post->comments) }}</td>
       </tr>
+      
+      <tr>
+        <th class="thead-dark">Tags</th>
+        <td>
+          @foreach ($post->tags as $key => $tag)
+            @if ($key == count($post->tags) - 1)
+              <span>{{ $tag->tag_name }}</span>
+            @else
+              <span>{{ $tag->tag_name }}, </span>
+            @endif
+          @endforeach
+        </td>
+      </tr>
     </table>
 
     <a class="btn btn-primary" href="{{ route("crud-articles.index") }}">Indietro</a>
